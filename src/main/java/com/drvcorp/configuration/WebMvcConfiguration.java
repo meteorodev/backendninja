52986b0671b2f33290dce25cca612918b06c165a
meteorodev <darwin11rv@gmail.com>
2018-01-30 21:44:20 -0500

    WebMvcConfigurer

--- src/main/java/com/drvcorp/configuration/WebMvcConfiguration.java
+++ src/main/java/com/drvcorp/configuration/WebMvcConfiguration.java
@@ -2,11 +2,13 @@ package com.drvcorp.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.drvcorp.component.RequestTimeInterseptor;
 
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer{
 	@Autowired
 	@Qualifier("requestTimeInterceptor")
